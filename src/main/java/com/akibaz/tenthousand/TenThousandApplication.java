b407210e28e83139515425a667a4b476cebfa3c7
akibaz <bavakumovic@gmail.com>
2024-10-14 23:09:05 +0200

    Setup Spring Boot project

---
+++ src/main/java/com/akibaz/tenthousand/TenThousandApplication.java
@@ -0,0 +1,13 @@
+package com.akibaz.tenthousand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenThousandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenThousandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akibaz/tenthousand/TenThousandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akibaz.tenthousand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenThousandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
